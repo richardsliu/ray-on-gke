582d92a5fc8595a0236d1ce8c4d5d9517f1faa94
Richard Liu <ricliu@google.com>
2023-01-26 23:03:49 +0000

    jupyterhub

---
+++ jupyterhub/jupyterhub.tf
@@ -0,0 +1,25 @@
+variable "kube_config" {
+  type    = string
+  default = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand(var.kube_config)
+  }
+}
+
+provider "kubernetes" {
+  config_path = pathexpand(var.kube_config)
+}
+
+resource "helm_release" "jupyterhub" {
+  name       = "jupyterhub"
+  repository = "https://jupyterhub.github.io/helm-chart"
+  chart      = "jupyterhub"
+
+  values = [
+    file("${path.module}/jupyterhub-values.yaml")
+  ]
+}
+
---
+++ jupyterhub/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.1"
+    }
+  }
+  //required_version = "~> 0.14"
+}
