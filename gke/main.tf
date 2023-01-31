bbdae4dfabeea0a8e82fb13a5027675972914cec
Richard Liu <39319471+richardsliu@users.noreply.github.com>
2023-01-30 17:24:30 -0800

    update oauth scopes

--- gke/main.tf
+++ gke/main.tf
@@ -70,6 +70,10 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
     ]
 
     labels = {
