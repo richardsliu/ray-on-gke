cb04c470ced0ffb31c943572d78f45ebf5eb1ab4
Richard Liu <39319471+richardsliu@users.noreply.github.com>
2023-02-09 14:58:42 -0800

    Update versions.tf

--- modules/jupyterhub/versions.tf
+++ modules/jupyterhub/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.8.0"
+      version = "~> 2.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
