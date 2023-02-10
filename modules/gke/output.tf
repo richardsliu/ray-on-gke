66a5391275dd25e73bcac291d17c790349cd75c9
Richard Liu <ricliu@google.com>
2023-02-10 00:10:35 +0000

    configure output tf

---
+++ modules/gke/output.tf
@@ -0,0 +1,19 @@
+output "host" {
+  description   =   "Kubernetes cluster endpoint"
+  value         =   "https://${data.google_container_cluster.my_cluster.endpoint}"
+  sensitive     =   true
+}
+
+output "token" {
+  description  =    "Kubernetes cluster token"
+  value        =    data.google_client_config.provider.access_token
+  sensitive    =    true
+}
+
+output "ca_certificate" {
+  description  =    "Kubernetes cluster ca certificate"
+  value        =    base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+  sensitive    =    true
+}
--- modules/kuberay/kuberay.tf
+++ modules/kuberay/kuberay.tf
@@ -1,16 +1,8 @@
-variable "kube_config" {
-  type    = string
-  default = "~/.kube/config"
-}
-
 provider "helm" {
   kubernetes {
-    config_path = pathexpand(var.kube_config)
-  }
-}
-
-provider "kubernetes" {
-  config_path = pathexpand(var.kube_config)
+    host      = var.host
+    token     = var.token
+    cluster_ca_certificate  = var.ca_certificate
 }
 
 resource "helm_release" "kuberay-operator" {
