4549decc605ed404d0be9de0279ec1ea7f4f9020
Richard Liu <ricliu@google.com>
2023-02-22 05:15:43 +0000

    add nvidia driver installer

--- main.tf
+++ main.tf
@@ -20,6 +20,15 @@ provider "kubernetes" {
     ) 
 }
 
+provider "kubectl" {
+  load_config_file       = false
+  host                   = data.google_container_cluster.ml_cluster.endpoint
+  token                  = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+      data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
+  )
+}
+
 provider "helm" {
   kubernetes {
     #config_path = pathexpand("~/.kube/config")
--- modules/kubernetes/kubernetes.tf
+++ modules/kubernetes/kubernetes.tf
@@ -1,3 +1,12 @@
+data "http" "nvidia_driver_installer_manifest" {
+  url = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml"
+}
+
+
+resource "kubectl_manifest" "nvidia_driver_installer" {
+  yaml_body = data.http.nvidia_driver_installer_manifest.body
+}
+
 resource "kubernetes_namespace" "ml" {
   metadata {
     name = var.namespace
