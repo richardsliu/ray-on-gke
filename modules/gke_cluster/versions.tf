9bbb7a029bed7da1671a39ccef50efb8da5b8c0d
Richard Liu <ricliu@google.com>
2023-04-05 20:33:56 +0000

    minor fixes

--- modules/gke_cluster/versions.tf
+++ modules/gke_cluster/versions.tf
@@ -1,12 +1,4 @@
 terraform {
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.8.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.18.1"
-    }
   }
 }
